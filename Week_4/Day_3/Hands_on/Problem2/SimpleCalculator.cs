f33ddaa70241e6d07b55d102b875799d0478f068
saikumar200112 <saikumarkagithala23@gmail.com>
2026-03-11 20:09:34 +0530

    Sai:Uploaded

---
+++ Week_4/Day_3/Hands_on/Problem2/SimpleCalculator.cs
@@ -0,0 +1,47 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter First Number");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Second Number");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Operater (+ - * /)");
+            char op = char.Parse(Console.ReadLine());
+            int c = 0;
+
+               switch (op)
+            {
+                     case '+':
+                        c = a + b;
+                        break;
+
+                case '-':
+                    c = a - b;
+                    break;
+
+                case '*':
+                    c = a * b;
+                    break;
+
+
+                case '/':
+                    c = a / b;
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid Operation");
+                    Console.ReadLine();
+                    return;
+            }
+            Console.WriteLine($"Final Result :{c}");
+            Console.ReadLine();
+              
+    
+        }
+    }
+}
