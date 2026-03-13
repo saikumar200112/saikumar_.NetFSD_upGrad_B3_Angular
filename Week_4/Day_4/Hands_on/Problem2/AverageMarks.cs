eb50b578318057c03637ea783412acc98a2b4bae
saikumar200112 <saikumarkagithala23@gmail.com>
2026-03-13 08:05:15 +0530

    Sai:Uploaded

---
+++ Week_4/Day_4/Hands_on/Problem2/AverageMarks.cs
@@ -0,0 +1,58 @@
+﻿namespace ConsoleApp1
+{
+    class Student
+    {
+
+
+        public double CalculateAverage(int m1, int m2, int m3)
+        {
+          
+            return (m1 + m2 + m3) / 3.0;
+        }
+        public string GetGrade(double average)
+        {
+            if (average >= 80)
+            {
+                return "A";
+            }
+            else if (average >= 70)
+            {
+                return "B";
+            }
+            else if (average >= 60)
+            {
+                return "C";
+            }
+            else if (average >= 50)
+            {
+                return "D";
+            }
+            else
+            {
+                return "Fail";
+            }
+        }
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("enter marks");
+            string str = Console.ReadLine();
+            string[] st = str.Split(" ");
+             a =int.Parse( st[0]);
+             b = int.Parse(st[1]);
+             c = int.Parse(st[2]);
+            Student s = new Student();
+            double avg = s.CalculateAverage(a,b,c);
+            string Grade = s.GetGrade(avg);
+            Console.WriteLine($"Average Marks :{avg}");
+            Console.WriteLine($"Grade :{Grade}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Week_4/Day_4/Hands_on/Problem3/ProductDetails.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace ProductManagement
+{
+    class Product
+    {
+        private int _ProductId;
+        private string _ProductName;
+        private int _Quantity;
+        private double _UnitPrice;
+
+        public Product(int ProductId)
+        {
+            this._ProductId = ProductId;
+        }
+        public int ProductId
+        {
+            get
+            {
+                return this._ProductId;
+            }
+        }
+        public string ProductName
+        {
+            get
+            {
+                return this._ProductName;
+            }
+            set
+            {
+                this._ProductName = value;
+            }
+        }
+        public int Quantity
+        {
+            get
+            {
+                return this._Quantity;
+            }
+            set
+            {
+               
+                    this._Quantity = value;
+  
+            }
+        }
+        public double UnitPrice
+        {
+            get
+            {
+                return this._UnitPrice;
+            }
+            set
+            {
+               
+                    this._UnitPrice = value;
+                
+                   
+            }
+        }
+        public void showproductDetails()
+        {
+            if (Quantity > 0)
+            {
+                double totalAmount = UnitPrice * Quantity;
+                Console.WriteLine($"ProductId :{ProductId}");
+                Console.WriteLine($"ProductName :{ProductName}");
+                Console.WriteLine($"Quantity :{Quantity}");
+                Console.WriteLine($"UnitPrice :{UnitPrice}");
+                Console.WriteLine($"Total Amount :{totalAmount}");
+
+
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Enter valid Quanty");
+            }
+
+        }
+
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p = new Product(101);
+            p.ProductName = "Apple";
+            p.Quantity = 3;
+            p.UnitPrice = 25000;
+            p.showproductDetails();
+        }
+    }
+}
