2890df7daedc0c373417c0701396017f1b41d078
saikumar200112 <saikumarkagithala23@gmail.com>
2026-03-16 08:59:40 +0530

    sai:uploaded

--- Week_4/Day_5/Hands_on/Problem1/Program.cs
+++ Week_4/Day_5/Hands_on/Problem1/Program.cs
@@ -1,18 +1,6 @@
 ﻿namespace ConsoleApp1
 {
-    class Calculator
-    {
-
-        public int Add(int a, int b)
-        {
-            ;
-            return a + b;
-        }
-        public int Subtract(int a, int b)
-        {
-
-            return a - b;
-        }
+    
 
 
     }
