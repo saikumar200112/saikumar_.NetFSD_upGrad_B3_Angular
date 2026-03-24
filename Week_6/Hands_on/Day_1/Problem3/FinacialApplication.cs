36d6582619f2178acc361ba8763133982a60f88e
saikumar200112 <saikumarkagithala23@gmail.com>
2026-03-24 20:23:37 +0530

    Sai:Uploaded

---
+++ Week_6/Hands_on/Day_1/Problem3/FinacialApplication.cs
@@ -0,0 +1,33 @@
+﻿using System;using System.IO;using System.Threading.Tasks;using System.Net.Http;namespace ConsoleApp8{    class Program    {        static async Task Main(string[] args)        {
+            Console.WriteLine("Financial Report Start");
+            DateTime starttime = DateTime.Now;
+;            Task task1 = Task.Run(() =>
+            {
+                GenerateSalesReport();
+            });
+            Task task2 = Task.Run(() =>
+            {
+                GenerateInventoryReport();
+            });
+            Task task3 = Task.Run(() =>
+            {
+                GenerateCustomerReport();
+            });
+            await Task.WhenAll(task1, task2, task3);
+            DateTime endtime = DateTime.Now;
+            TimeSpan totalTime = endtime - starttime;
+            Console.WriteLine("All Reports Completed");
+            Console.WriteLine($"Total Processing Time:{totalTime.Seconds} Seconds");
+            Console.ReadLine();        }
+        static void GenerateSalesReport()
+        {
+            Console.WriteLine("[start] Generating Sales Report");            Task.Delay(2000).Wait();            Console.WriteLine("[finsh] Sales Report Ready");        }
+        static void GenerateInventoryReport()
+        {
+            Console.WriteLine("[start] Generating Inventory Report");            Task.Delay(2000).Wait();            Console.WriteLine("[finsh] Inventory Report Ready");        }
+        static void GenerateCustomerReport()
+        {
+            Console.WriteLine("[start] Generating Customer Report");            Task.Delay(2000).Wait();            Console.WriteLine("[finsh] Customer Report Ready");        }
+                
+    }
+}
---
+++ Week_6/Hands_on/Day_1/Problem5/OrderTracing.cs
@@ -0,0 +1,34 @@
+﻿using System;using System.IO;using System.Threading.Tasks;using System.Net.Http;using System.Diagnostics;using System.Reflection;namespace ConsoleApp8{    class Program    {        static async Task Main(string[] args)        {
+            string order_trace = "order_trace.txt";
+            TextWriterTraceListener fileLog = new TextWriterTraceListener(order_trace);
+
+            Trace.Listeners.Add(fileLog);
+            Trace.AutoFlush = true;
+
+            Trace.WriteLine("start order processing");
+            try
+            {
+                ProcessOrder();
+                Trace.TraceInformation("Order completed successfully");
+
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError($"[Message]:{ex.Message}");
+            }
+            Console.WriteLine("Processing Finished");
+            Trace.Flush();
+
+            Console.ReadLine();        }
+        static void ProcessOrder()
+        {
+            Trace.WriteLine("step 1: Validating Order");
+            Trace.WriteLine("step 2: Processing Payment");
+            Trace.WriteLine("step 3: Updating Inventory");
+            throw new Exception("database connection during inventory update");
+            Trace.WriteLine("step 4: Generating Invoice");
+        }
+
+
+    }
+}
