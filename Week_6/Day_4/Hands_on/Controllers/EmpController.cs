2e7c63361bd91165fcb1ac1c4c31967da4ac2405
saikumar200112 <saikumarkagithala23@gmail.com>
2026-03-26 20:15:44 +0530

    Sai:Uploaded

---
+++ Week_6/Day_4/Hands_on/Controllers/EmpController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class EmpController : Controller
+    {
+        public IActionResult Index()
+        {
+            Employee empobj1 = new Employee() { Empno =101,Ename="Saikumar",Job="Developer",Salary=50000,Deptno=10 };
+            Employee empobj2 = new Employee() { Empno = 101, Ename = "Jaswanth", Job = "Analyst", Salary = 70000, Deptno = 10 };
+            Employee empobj3 = new Employee() { Empno = 101, Ename = "Satish", Job = "Web-Developer", Salary = 60000, Deptno = 10 };
+            Employee empobj4 = new Employee() { Empno = 101, Ename = "Vinod", Job = "Android-Developer", Salary = 65000, Deptno = 10 };
+            List<Employee> empList = new List<Employee>();
+            empList.Add(empobj1);
+            empList.Add(empobj2);
+            empList.Add(empobj3);
+            empList.Add(empobj4);
+            return View(empList);
+        }
+    }
+}
---
+++ Week_6/Day_4/Hands_on/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+       public List<Products> GetProducts()
+        {
+            List<Products> Prodlist = new List<Products>();
+            Products p1 = new Products {PId=1,Pname="samsung",    Price=40000, Category="Electronics",Stock=10};
+            Products p2 = new Products { PId = 2, Pname = "sony", Price = 35000, Category = "Electronics", Stock = 10 };
+            Products p3 = new Products { PId = 3, Pname = "vivo", Price = 30000, Category = "Electronics", Stock = 10 };
+            Products p4 = new Products { PId = 4, Pname = "oppo", Price = 20000, Category = "Electronics", Stock = 10 };
+            Prodlist.Add(p1);
+            Prodlist.Add(p2);
+            Prodlist.Add(p3);
+            Prodlist.Add(p4);
+            return Prodlist;
+        }
+        public IActionResult Index()
+        {
+            List<Products> products = GetProducts();
+       
+            return View(products);
+        }
+       
+        public IActionResult Details(int id)
+        {
+            List<Products> products = GetProducts();
+           
+            Products Product = products.FirstOrDefault(p => p.PId == id);
+          
+            return View(Product);
+        }
+    }
+}
